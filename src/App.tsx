cd0e8564171dfd61e08f1c9b4072daa737e14e09
Matt Wilde <matt.wilde@elastic.co>
2025-11-22 10:23:06 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h5>Hello world</h5>
+      <h5>Hello worlds</h5>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
