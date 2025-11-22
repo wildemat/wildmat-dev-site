6319ef26681ea7ef55005358f7eccca45b46d3dd
Matt Wilde <matt.wilde@elastic.co>
2025-11-22 11:04:52 -0500

    contact

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ const App = () => {
             <h1>wildmat.dev</h1>
           </div>
         </section>
+        {/* Footer */}
+        <footer className="py-6">
+          <div className="container">
+            <p>Contact: hello@wildmat.dev</p>
+            <p>&copy; {new Date().getFullYear()} wildmat.dev</p>
+            <p>All rights reserved.</p>
+            <p>
+              <a href="https://github.com/wildemat">GitHub</a>
+              <a href="https://linkedin.com/in/wildematt">LinkedIn</a>
+            </p>
+          </div>
+        </footer>
       </main>
     </div>
   );
